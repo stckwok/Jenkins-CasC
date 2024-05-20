bc524d2b84d7689967e23c1a8c069d6cdd8bfc78
Samuel Kwok <stckwok@hotmail.com>
2024-05-19 17:19:56 -0700

    attach a volume

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 echo $ADMIN_ID
 echo $ADMIN_PWD
 echo $NODE_URL
-docker run --name jenkins --rm -p 8080:8080 --env NODE_URL=$NODE_URL --env JENKINS_ADMIN_ID=$ADMIN_ID --env JENKINS_ADMIN_PASSWORD=$ADMIN_PWD jenkins:jcasc
+docker run --name jenkins --rm -p 8080:8080 -v jenkins_home:/var/jenkins_home --env NODE_URL=$NODE_URL --env JENKINS_ADMIN_ID=$ADMIN_ID --env JENKINS_ADMIN_PASSWORD=$ADMIN_PWD jenkins:jcasc
